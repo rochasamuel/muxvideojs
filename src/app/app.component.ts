68f5540d068d4b232d5d07351af6f1912539e387
Samuel Rocha <samuelsilv.rocha@gmail.com>
2023-03-05 11:31:36 -0300

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mux';
+
+  public urls: string[] = [
+    'pLfxbhKo00DOWGRQ4hb27Yug56kb84a5PUs02tyPpg9KE',
+    '02PmN5CFIblo7C3lbdppxQAPueHGBlf02EiJItI01FZads',
+    'Ev7ssJjkd00igH8W5l800Ksozf36jZvmj5h96dyNa1l02k',
+    'sVGG4ew7FxrmBohRQwhoyuX5bvV8db2iRA3w5Zk4FrM',
+    'UX3TDhygHUbJLelxbZlwgrSDJyAGnwQe01BPzcAP45wg',
+    'SotY8IrPjx9kRuWJIzIr1DIK1aswvu01mOH24FYiNEuI',
+    'agj0002LbI01q32gMJXCr1Qt8I1Yx02a38FvxtWdKy7Txb4',
+    '1cvyTraoBN9TET9hXoxABoNuu02IN9HmAHashUGJuU4Y'
+  ];
 }
